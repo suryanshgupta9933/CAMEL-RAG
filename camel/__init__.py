1d0059b02c14a4c5b18582317fa5bcb3c9fa0255
Suryansh Gupta <suryanshgupta9933@gmail.com>
2024-06-04 18:24:29 +0530

    proper camel implementation

--- camel/__init__.py
+++ camel/__init__.py
@@ -1,9 +1,3 @@
-from .config import (assistant_role_name,
-                        user_role_name,
-                        task)
 from .camel import CAMELAgent
-from .prompt import (task_specifier_sys_msg,
-                    task_specifier_msg,
-                    assistant_inception_prompt,
-                    user_inception_prompt)
-from .utils import get_sys_msgs
+from .prompt import task_specifier_prompts, inception_prompts, get_sys_msgs
+from .chat import get_specified_task, camel_chat
--- camel/chat.py
+++ camel/chat.py
@@ -4,37 +4,16 @@ from langchain_openai import ChatOpenAI
 from langchain.schema import HumanMessage
 
 from camel import CAMELAgent
-from config import assistant_role_name, user_role_name, task
-from prompt import task_specifier_sys_msg, task_specifier_msg, get_sys_msgs
 
 # Create a Task Specifing Agent for Brainstorming
-task_specify_agent = CAMELAgent(task_specifier_sys_msg, ChatOpenAI(model="gpt-3.5-turbo-1106", temperature=0.0))
-specified_task_msg = task_specify_agent.step(task_specifier_msg)
-specified_task = specified_task_msg.content
-
-# Get System and User Inception Messages
-assistant_sys_msg, user_sys_msg = get_sys_msgs(assistant_role_name, user_role_name, specified_task)
-
-# Create an Assistant Agent for Inception
-assistant_agent = CAMELAgent(assistant_sys_msg, ChatOpenAI(model="gpt-3.5-turbo-1106", temperature=0.0))
-
-# Create a User Agent for Inception
-user_agent = CAMELAgent(user_sys_msg, ChatOpenAI(model="gpt-3.5-turbo-1106", temperature=0.0))
-
-# Reset agents
-assistant_agent.reset()
-user_agent.reset()
-
-# Initialize chats
-assistant_msg = HumanMessage(
-    content=(
-        "Now start to give me instructions one by one. "
-        "Only reply with Instruction and Input."
-    )
-)
+def get_specified_task(task_specifier_sys_msg, task_specifier_msg):
+    task_specify_agent = CAMELAgent(task_specifier_sys_msg, ChatOpenAI(model="gpt-3.5-turbo-1106", temperature=0.0))
+    specified_task_msg = task_specify_agent.step(task_specifier_msg)
+    specified_task = specified_task_msg.content
+    return specified_task
 
 # Camel Chat Function
-def camel_chat():
+def camel_chat(user_agent, assistant_agent, assistant_msg):
     chat_turn_limit, n = 10, 0
     assistant_response =[]
     while n < chat_turn_limit:
@@ -60,4 +39,7 @@ def camel_chat():
     except Exception as e:
         print(e)
 
-    return camel_response
+    with open("camel_response.txt", "w") as f:
+        f.write(camel_response)
+
+    return camel_response 
