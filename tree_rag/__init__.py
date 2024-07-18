693b8d100eb7e4c0dca04593c0dd38007b62d46f
Suryansh Gupta <suryanshgupta9933@gmail.com>
2024-07-18 19:47:47 +0530

    removed

--- tree_rag/__init__.py
+++
@@ -1,3 +0,0 @@
-from .prompt import get_entity_retriever_prompt, get_search_terms_retriever_prompt
-from .chain import get_llm_chain
-from .utils import get_filtered_urls
--- tree_rag/chain.py
+++
@@ -1,9 +0,0 @@
-# Importing Dependencies
-from langchain_openai import ChatOpenAI
-
-# Create the LLM chain
-def get_llm_chain(prompt):
-    # Create the LLM chain
-    llm = ChatOpenAI(model="gpt-3.5-turbo-1106", temperature=0.0)
-    llm_chain = prompt | llm
-    return llm_chain
